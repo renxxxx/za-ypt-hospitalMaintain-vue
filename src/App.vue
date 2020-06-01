f174590312746031f7bd4844fffa6f1256075650
xuxk13046567620 <839362863@qq.com>
2020-06-01 11:33:39 +0800

    ypt

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
+  width: 100%;
+  height: 100vh;
 }
 body{
   padding: 0;

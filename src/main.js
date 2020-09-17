008da2dde04dbfbbc44811dd2d95de5af2ebb70d
BlockHead <1955372313@qq.com>
2020-09-17 11:32:08 +0800

    1.0.2009171132

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import axios from 'axios'
 import qs from 'qs'; 
 moment.locale('zh-cn');
 Vue.prototype.moment = moment;
-Vue.prototype.$version = '1.0.200917117'
+Vue.prototype.$version = '1.0.2009171132'
 // import VueAxios from 'vue-axios'
 
 // Vue.use(VueAxios,axios);

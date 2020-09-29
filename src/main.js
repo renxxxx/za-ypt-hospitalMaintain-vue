173c36de176fc97e4596c0b2ba7e912deb286270
inininininin <413038044@qq.com>
2020-09-29 14:43:33 +0800

    1.0.1.2009291440

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import axios from 'axios'
 import qs from 'qs';  
 moment.locale('zh-cn');
 Vue.prototype.moment = moment;
-Vue.prototype.$version = '1.0.2009191450'
+Vue.prototype.$version = '1.0.1.2009291440'
 // import VueAxios from 'vue-axios'
 
 // Vue.use(VueAxios,axios);
